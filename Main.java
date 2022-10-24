2e1a0a5c6839da58ea94c3bfc440e1bec824d395
Steicie Aleixo Sousa <51095043+Staici@users.noreply.github.com>
2022-10-24 11:51:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int num[]=new int[5];
+    num[0]=1;
+     num[1]=2;
+     num[2]=3;
+     num[3]=4;
+     num[4]=5;
+    for(int i=0;i<=num.length;i++)//num.length(Já adiciona o numeor de vetores definidos)
+    {
+      System.out.println("%d"+num[0]);
+      
+    }
+  }
+}
